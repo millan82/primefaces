ed02d805a52be0b597df3a34b8dc2600f3b05601
ATSISTEMAS\mmillan <mmillan@atsistemas.com>
2018-12-05 09:27:04 +0100

    nuevo repo

---
+++ primefaces/src/main/java/es/jsfprime/primefaces/PrimefacesApplication.java
@@ -0,0 +1,12 @@
+package es.jsfprime.primefaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimefacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimefacesApplication.class, args);
+	}
+}
---
+++ primefaces/src/test/java/es/jsfprime/primefaces/PrimefacesApplicationTests.java
@@ -0,0 +1,16 @@
+package es.jsfprime.primefaces;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrimefacesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
